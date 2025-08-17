1b6236394fe24231ca8df45298377dab8ec7b50b
ほたまちスバル <93361361+minamikana-git@users.noreply.github.com>
2025-08-17 10:58:12 +0900

    LuckPermsUtil クラスを追加 - プレイヤーの Prefix を安全に取得する safePrefix メソッドを実装 - LuckPerms 未ロード時や非依存環境への対応を追加

---
+++ src/main/java/org/hotamachisubaru/miniutility/util/LuckPermsUtil.java
@@ -0,0 +1,22 @@
+package org.hotamachisubaru.miniutility.util;
+
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedMetaData;
+import org.bukkit.entity.Player;
+
+public final class LuckPermsUtil {
+
+    private LuckPermsUtil() {} // インスタンス化禁止
+
+    public static String safePrefix(Player player) {
+        try {
+            var api = LuckPermsProvider.get();
+            CachedMetaData meta = api.getPlayerAdapter(Player.class).getMetaData(player);
+            return meta.getPrefix() == null ? "" : meta.getPrefix();
+        } catch (IllegalStateException | NoClassDefFoundError e) {
+            // LuckPerms が無い / まだロードされていない時
+            return "";
+        }
+    }
+}
+
